2f5d1c2871509f7f91000e2855b9bf6460b25cb5
XieL <xielu1989@sina.com>
2023-11-03 22:14:25 +0800

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
-install.packages("Seurat",version="igraph", version="1.5.1")
-install.packages("Seurat",version="4.4.0")
+#install.packages("Seurat",version="igraph", version="1.5.1")
+install.packages("Seurat",version="3.0.2")
 install.packages("dplyr")
